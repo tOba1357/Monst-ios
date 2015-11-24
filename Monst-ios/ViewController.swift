f10498a4516c7efe40f738006d4f39a243e02971
Tatsuya Oba <toba17893@gmail.com>
2015-11-24 11:30:54 +0900

    first commmit

---
+++ Monst-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Monst-ios
+//
+//  Created by Tatsuya Oba on 2015/11/24.
+//  Copyright © 2015年 Tatsuya Oba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
